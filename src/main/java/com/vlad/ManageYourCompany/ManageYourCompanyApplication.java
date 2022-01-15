e3bd706426bf004f59117789a34b1ca4a4b22f40
vlad-dieaconu <vladdieaconu@yahoo.com>
2022-01-15 15:55:22 +0200

    First commit

---
+++ src/main/java/com/vlad/ManageYourCompany/ManageYourCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.vlad.ManageYourCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageYourCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageYourCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlad/ManageYourCompany/ManageYourCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlad.ManageYourCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageYourCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
