bcdf4082a312b141cf77c04b45174fbd4d26e9ea
vlad-dieaconu <vladdieaconu@yahoo.com>
2022-01-24 20:24:52 +0200

    [MYC-3] I've setup EmailService and first time login user feature.

---
+++ src/main/java/com/vlad/ManageYourCompany/services/EmailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.vlad.ManageYourCompany.services;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl {
+
+    private JavaMailSender javaMailSender;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(String toEmail, String subject, String message) {
+
+        var mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(toEmail);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(message);
+
+        mailMessage.setFrom("manageyourcompany@rest.com");
+
+        javaMailSender.send(mailMessage);
+    }
+}
